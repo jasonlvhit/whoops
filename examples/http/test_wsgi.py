b0e5434ae7b902c6d3ca8658943342bf3840883e
skyline75489 <skyline75489@outlook.com>
2015-02-17 10:48:32 +0800

    add WSGI server example

---
+++ examples/http/test_wsgi.py
@@ -0,0 +1,17 @@
+from wsgi_server import make_server
+
+HELLO_WORLD = b"Hello World!\n"
+
+
+def my_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    path = environ['PATH_INFO']
+    if path == '' or path == '/':
+        return [HELLO_WORLD]
+    else:
+        pass
+
+server = make_server('127.0.0.1', 5000, my_app)
+server.listen()
